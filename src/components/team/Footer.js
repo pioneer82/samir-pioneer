d5ad83adb01951d62b4d8516b7294dc3000fa9f9
pioneer82 <pioneer200082@gmail.com>
2025-06-20 11:32:19 +0300

    compoments changed

--- src/components/team/Footer.js
+++ src/components/team/Footer.js
@@ -2,34 +2,33 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useSelector } from 'react-redux';
 
-const DEV_TEXT = {
-  English: 'Designed and Developed by Samir and Tom',
-  Japanese: 'サミールとトムによる設計と開発'
-};
-
-const COPYRIGHT_TEXT = {
-  English: 'Copyright',
-  Japanese: '著作権'
+const TEXT = {
+  dev: {
+    English: 'Designed and Developed by Samir and Tom',
+    Japanese: 'サミールとトムによる設計と開発'
+  },
+  copyright: {
+    English: 'Copyright',
+    Japanese: '著作権'
+  },
+  brand: 'Zentech Collective'
 };
 
 function Footer() {
   const year = new Date().getFullYear();
   const language = useSelector(state => state.language.language);
 
-  const devText = language === 'English' ? DEV_TEXT.English : DEV_TEXT.Japanese;
-  const copyrightText = language === 'English' ? COPYRIGHT_TEXT.English : COPYRIGHT_TEXT.Japanese;
-
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>{devText}</h3>
+          <h3>{TEXT.dev[language] || TEXT.dev.English}</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>{copyrightText} © {year}</h3>
+          <h3>{TEXT.copyright[language] || TEXT.copyright.English} © {year}</h3>
         </Col>
         <Col md="4" className="footer-body">
-          <h3>Zentech Collective</h3>
+          <h3>{TEXT.brand}</h3>
         </Col>
       </Row>
     </Container>
