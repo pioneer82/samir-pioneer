6d22c6cb30f14f5180053467340b511b48093c64
pioneer82 <pioneer200082@gmail.com>
2025-06-20 11:05:29 +0300

    code change

--- src/components/team/Footer.js
+++ src/components/team/Footer.js
@@ -1,32 +1,32 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
-
 import { useSelector } from 'react-redux';
 
+const DEV_TEXT = {
+  English: 'Designed and Developed by Samir and Tom',
+  Japanese: 'サミールとトムによる設計と開発'
+};
+
+const COPYRIGHT_TEXT = {
+  English: 'Copyright',
+  Japanese: '著作権'
+};
+
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
+  const year = new Date().getFullYear();
   const language = useSelector(state => state.language.language);
-  const engDev = 'Designed and Developed by Samir and Tom';
-  const japDev = 'サミールとトムによる設計と開発'
+
+  const devText = language === 'English' ? DEV_TEXT.English : DEV_TEXT.Japanese;
+  const copyrightText = language === 'English' ? COPYRIGHT_TEXT.English : COPYRIGHT_TEXT.Japanese;
+
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>
-            {
-              language === 'English' ? engDev : japDev
-            }
-          </h3>
+          <h3>{devText}</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>{language === 'English' ? 'Copyright' : '著作権'} © {year}</h3>
+          <h3>{copyrightText} © {year}</h3>
         </Col>
         <Col md="4" className="footer-body">
           <h3>Zentech Collective</h3>
